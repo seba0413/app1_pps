298223760d5374575eb6d832d46501699c4f94e7
seba0413 <sebastianlpasquini@gmail.com>
2020-04-25 19:42:55 -0300

    correcciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { CameraService } from '../servicios/camera.service';
 import { ModalInfoService } from '../helpers/modal-info.service';
+import { AuthService } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,8 @@ export class HomePage {
   constructor( public actionSheetController: ActionSheetController,
                private router: Router,
                private cameraService: CameraService,
-               private modalInfoService: ModalInfoService ) {}
+               private modalInfoService: ModalInfoService,
+               private  AFauth: AuthService) {}
 
   sacarFoto( categoria: number ) {
     this.cameraService.sacarFoto()
@@ -30,11 +32,15 @@ export class HomePage {
   subirFoto( datosImagen, categoria ) {
     this.cameraService.guardarFoto( datosImagen, categoria )
     .then(() => {
-      this.modalInfoService.abrirModalInfo('Foto duardada', 'success');
+      this.modalInfoService.abrirModalInfo('Foto guardada', 'success');
     })
     .catch( error => {
       this.modalInfoService.abrirModalInfo('Error al guardar la foto', 'error');
     });
+  }
+
+  OnLogOut() {
+    this.AFauth.logout();
   }
 
   async presentActionSheet() {
@@ -53,8 +59,8 @@ export class HomePage {
           role: 'destructive',
           icon: 'log-out',
           handler: () => {
-            // this.OnLogOut();
-          },
+            this.OnLogOut();
+          }
         }
       ]
     });
